21800301a04014e97007d76cc72cd1d4e8cfd0bc
Md. Aiful Alam Mahim <arifulalam7865@gmail.com>
2025-09-03 19:42:55 +0600

    tiny change in script file

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
-
 function toggleDarkMode() {
   const toggle = document.getElementById("darkModeToggle");
   const icon = document.getElementById("mode-icon");
